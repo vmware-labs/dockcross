21fdaf8b9e3309904e4d29846abb105ea927b4db
Matt McCormick <matt.mccormick@kitware.com>
2022-11-30 15:41:47 -0500

    manylinux_2_28-x64: Update for gcc-toolset-12
    Closes #746

--- manylinux_2_28-x64/Toolchain.cmake
+++ manylinux_2_28-x64/Toolchain.cmake
@@ -5,7 +5,7 @@ set(CMAKE_SYSTEM_PROCESSOR x86_64)
 
 set(MANYLINUX_2_28 TRUE)
 
-set(CROSS_ROOT /opt/rh/gcc-toolset-11/root/bin/)
+set(CROSS_ROOT /opt/rh/gcc-toolset-12/root/bin/)
 set(CMAKE_C_COMPILER ${CROSS_ROOT}/gcc)
 set(CMAKE_CXX_COMPILER ${CROSS_ROOT}/g++)
 set(CMAKE_ASM_COMPILER ${CROSS_ROOT}/as)
