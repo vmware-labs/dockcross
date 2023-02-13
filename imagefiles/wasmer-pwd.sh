ae03014632ce219931202df83a9b90ab59de497d
Matt McCormick <matt.mccormick@kitware.com>
2023-02-12 22:38:02 -0500

    web-wasi: mount /work in emulator for access to working directory
    Ensure there is access to the source tree for try_run's.

--- imagefiles/wasmer-pwd.sh
+++ imagefiles/wasmer-pwd.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-# Mount the PWD to enable access in try_run commands
-exec ${WASMER_DIR}/bin/wasmer run --dir=. --dir=$PWD "$@"
+# Mount the PWD and the /work mount dir to enable access in try_run commands
+exec ${WASMER_DIR}/bin/wasmer run --dir=. --dir=$PWD --dir=/work/ "$@"
